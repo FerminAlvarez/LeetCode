a0bda809adbef31e950b7b8ee06064444dec10b4
FerminAlvarez <40571267+FerminAlvarez@users.noreply.github.com>
2022-07-29 17:41:15 -0300

    Time: 341 ms (56.32%), Space: 16.3 MB (49.09%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,28 +1,17 @@
 class Solution:
     def sortedSquares(self, nums):
-        result = [nums[0]**2]
+        result = []
+        left = 0
+        right = len(nums) - 1
         
-        for i in range (1, len(nums)):
-            square = nums[i]**2
-            position = self.search(result, square)
-            result.insert(position,square)
-            
-        return result
-        
-    
-    def search(self, nums, target):
-        return self.binarySearch(nums, 0, len(nums) - 1, target)
-    
-    
-    def binarySearch(self, nums, first, last, target):
-        
-        if(first > last):
-            return first
-            
-        mid = (first + last) // 2
-            
-        if(target < nums[mid]): return self.binarySearch(nums, first, mid - 1, target)
-            
-        if(target > nums[mid]): return self.binarySearch(nums, mid + 1, last, target)
-        
-        return mid
+        while left <= right:
+            rightSquare = nums[right] **2
+            leftSquare = nums[left] **2
+            if leftSquare > rightSquare:
+                result.append(leftSquare)
+                left += 1
+            else:
+                result.append(rightSquare)
+                right -= 1
+                
+        return result[::-1]
