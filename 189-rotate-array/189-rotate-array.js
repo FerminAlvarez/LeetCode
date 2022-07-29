c6f192f098ae5f60c40eec51a650d0d7f7937b48
FerminAlvarez <40571267+FerminAlvarez@users.noreply.github.com>
2022-07-28 22:58:23 -0300

    Time: 132 ms (59.70%), Space: 51.8 MB (59.36%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    nums.push(...nums.splice(0, nums.length - k % nums.length));
+    return nums;
+};
